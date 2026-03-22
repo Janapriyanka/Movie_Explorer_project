4c08c5eb1aef933cab845cb3bd71a8312c7ff26d
Hariharan-Elumalai <hariharanelumalai947@gmail.com>
2026-03-22 09:51:10 +0530

    added collection-header

--- collections.js
+++ collections.js
@@ -204,3 +204,10 @@ displayMovies(filteredMovies);
 });
 
 }
+
+const menuToggle = document.getElementById("menuToggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
